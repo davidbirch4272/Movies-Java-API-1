7e1ccce8c7a5007ad1ccc0afad1c04a71a2933e8
davidbirch4272 <davidbirch4272@gmail.com>
2025-05-08 11:12:00 -0400

    changes

--- ap.js
+++ ap.js
@@ -1,30 +1,8 @@
-// https://www.omdbapi.com/?apikey=b5972967&s=fast&Furious
+function openMenu() {
+  document.body.classList += " menu--open"
+  console.log(openMenu)
+}
 
-const movieListEl = document.querySelector(".movies__Wrapper");
-
-async function main() {
-const movies = (await fetch("https://www.omdbapi.com/?apikey=b5972967&s=fast&Furious"));
-const moviesData = await movies.json();
-movieListEl.innerHTML = moviesData.map((imdbID) => userHTML(imdbID)).join("");
-}      
-      
-      
-main();      
-
-function showMovies(imdbID) {
-    localStorage.setItem("imdbID", imdbID);
-    window.location.href = `${window.location.origin}/selector.html`
-    }
-      
-function userHTML(imdbID) {       
-      return `<div class="container">
-      <div class="user-card__container">
-        <img src=${imdbID.Poster}</img>
-        <h2>${imdbID.Title}</h2>
-        <p>${imdbID.Year}</p>
-        <p>${imdbID.Type}</p>
-        <p>${imdbID.imdbID}</p>
-        </div>
-        </div>`;
-    
-  }
+function closeMenu() {
+  document.body.classList.remove('menu--open')
+}
--- selector.js
+++ selector.js
@@ -1,6 +1,4 @@
 //https://www.omdbapi.com/?apikey=b5972967&s=new
-let movies;
-let Search;
 
 async function renderMovies(filter) {
 
@@ -8,30 +6,29 @@ async function renderMovies(filter) {
 
   const movieListEl = document.querySelector(".movies__Wrapper");
 
-  async function main() {
-  const movies = (await fetch(`https://www.omdbapi.com/?apikey=b5972967&s=${ movieTitle }`));
+  
+  const movies = await fetch(`https://www.omdbapi.com/?apikey=b5972967&s=${ movieTitle }`);
   const moviesData = await movies.json();
   console.log(moviesData)
-  movieListEl.innerHTML = moviesData.Search.map((imdbID) => userHTML(imdbID)).join("");
-  }
-  
-  movieListEl.classList += ' movies__loading'
 
-if (!movies) {
-  movies = await main();
+
+movieListEl.classList += ' movies__loading'
+
+movieListEl.classList.remove('movies__loading')
+
+if(filter === 'NEW_TO_OLD') {
+  moviesData.Search.sort((a, b) => b.Year - a.Year)
+}
+if(filter === 'OLD_TO_NEW') {
+  moviesData.Search.sort((a, b) => a.Year - b.Year)
 }
 
-  movieListEl.classList.remove('movies__loading')
+movieListEl.innerHTML = moviesData.Search.map((imdbID) => userHTML(imdbID)).join("");
 
-if (filter === 'Year') {
-   Search.sort((a, b) => a.Year - b.Year);
-   }
-else if (filter === 'Title') {
-   Search.sort((a, b) => a.Title - b.Title);
-  }
+} 
 
-  function userHTML(imdbID) {
-
+function userHTML(imdbID) {
+  
     return `<div class="movie">
     <figure class="movies__wrapper">
     <img class="movie__poster" src="${imdbID.Poster}" alt="">
@@ -48,9 +45,6 @@ else if (filter === 'Title') {
     </div>`
     }
   
-  movieListEl.innerHTML = moviesHtml;
-} 
-
 function filterMovies(event) {
   renderMovies(event.target.value);
   }
